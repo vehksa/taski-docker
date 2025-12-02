bb958898dba6cef11b273969de4d861e607ca7cd
Anastasia <hveksa@gmail.com>
2025-12-02 15:13:47 +0300

    testflake8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +8,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
